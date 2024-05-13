7b1985670dc34331b0b80702cbaf423254432e70
Kishore Murugan <kishoremurgan0077@gmail.com>
2024-05-14 02:03:10 +0530

    3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8888;
 const bodyParser = require("body-parser");
-const productRoutes = require("./Routes/ProductRoutes");
+const productRoutes = require(".src/Routes/ProductRoutes");
 
 app.use(cors());
 app.use(bodyParser.json());
