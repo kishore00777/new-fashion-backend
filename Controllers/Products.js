44b386fd2b2046c8b0eb21026376b1ae2c5fd7ad
Kishore Murugan <kishoremurgan0077@gmail.com>
2024-05-14 02:24:09 +0530

    d

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const GetAllProducts = () => {
-  const filepath = path.join(__dirname, "./Data", "Product.json");
+  const filepath = path.join(__dirname, "./Data/Product.json");
   const Data = fs.readFileSync(filepath);
   return JSON.parse(Data);
 };
